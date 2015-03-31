76ba6341a19672473b728acd7b7437cf6f0f1aa1
Tarheelbrady <bradyratchford@gmail.com>
2015-03-31 13:14:12 -0400

    Committed by Udacity

---
+++ Pitch Perfect 2.0/RecordedAudio.swift
@@ -0,0 +1,15 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect 2.0
+//
+//  Created by Brady Ratchford on 3/16/15.
+//  Copyright (c) 2015 Brady Ratchford. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    
+    var filePathUrl: NSURL!
+    var title: String!
+}
